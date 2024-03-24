1800cc8de9a5a07ccb1bed229927a1eece6dae0d
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-03-24 14:36:19 +0700

    Make hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="text-center">
+      <Hero />
       <h1>This is Home</h1>
+      <Link href="/dashboard">
+        <Button>Click me</Button>
+      </Link>
     </main>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,5 @@
+export default function Hero() { return (
+    <div className="flex items-center justify-center h-[70vh] bg-slate-700">
+        <h1 className="text-white text-4xl font-bold">Welcome to Learn Kinde</h1>
+    </div>
+); }
