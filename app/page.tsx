3a7f6b980f0e6aab3a17da5e1515e6c654874eda
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-03-24 16:42:38 +0700

    Wrap layout by KindeProvider tag

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
+  
   return (
     <main className="text-center">
       <Hero />
